8ac237d6871589e9bb37d0ed1038cd9858e7a820
Bjarne Matz <65752490+BjarneMatz@users.noreply.github.com>
2024-03-23 23:10:59 +0100

    Create config-example.py

---
+++ config-example.py
@@ -0,0 +1,8 @@
+version = "1.20.4" # accepts common minecraft versions like 1.8.9, 1.12.2, etc.
+absolute_path = "/home/server/" # absolute path to server root
+auto_accept_eula = "false" # read minecrafts EULA!
+max_ram = "10G" # directly passed to start file, common values from java accepted here
+min_ram = "10G" # same here
+server_file_name = "server.jar"
+start_file_name = "start.sh" # currently simple shell file is generated to start server later
+start_server_after_setup = "False" # execute shell file when finished
